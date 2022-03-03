74b4b3d992edfa08ba686d9268a4f066bba18d55
Mohak-Tripathi <mohaktripathi029@gmail.com>
2022-03-03 09:41:02 -0800

    done
    node

---
+++ Day1(Module)/main.js
@@ -0,0 +1,21 @@
+const addition = require("./add")
+
+const multiplication = require("./multiply")
+
+const division = require("./divide")
+
+const substraction = require("./substract")
+
+
+let add1= addition(4,7);
+console.log(add1);
+
+let mult1= multiplication(4,7);
+console.log(mult1);
+
+let div1= division(4,7)
+console.log(div1);
+
+let substraction1= substraction(70,2);
+console.log(substraction1);
+
