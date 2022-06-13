3686a7860ab3eda533bb136316c2115126ec4d1c
Mohak-Tripathi <mohaktripathi029@gmail.com>
2022-06-13 18:26:14 +0530

    custom validation added

--- validator1/src/controller/user.controller.js
+++ validator1/src/controller/user.controller.js
@@ -1,53 +1,68 @@
-const express= require("express")
+const express = require("express");
 
+const { body, validationResult } = require("express-validator"); //ye likhana padata hai
 
-const { body, validationResult } = require("express-validator");
-const User = require("../models/user.models")
+const User = require("../models/user.models");
 
-const router = express.Router()
+const router = express.Router();
 
+router.post(
+  "",
 
+  body("first_name")
+    .not()
+    .isEmpty()
+    .withMessage("first name is required and should not be empty"),
+  body("last_name")
+    .not()
+    .isEmpty()
+    .withMessage("Last Name should not be empty!!"),
+  body("email").isEmail().withMessage("Please enter valid email address").custom(async()=>{
+      const user= await User.findOne({email:value})
 
-router.post("",
-
-body("first_name").not().isEmpty().withMessage("first name is required and should not be empty"),
-body("last_name").not().isEmpty().withMessage("Last Name should not be empty!!"),
-body("email").isEmail().withMessage('Please enter valid email address'),
-body("pincode").not().isEmpty().withMessage("pincode should not be empty").isLength({min:6, max:6}).withMessage("Pincode should consist of 6 digits"),
-body("age").not().isEmpty().withMessage("age should not be empty").isInt({min:1, max:100}).withMessage('Age should between 1 to 100'),
-body("gender").custom( (value)=>{
-
-    if(value=="Male" || value=="Female" || value=="Others"){ 
-        return true;
+      if(user){
+          throw new Error ("Email is already taken")
+      }
+      else{
+          return true
+      }
+  }),
+  body("pincode")
+    .not()
+    .isEmpty()
+    .withMessage("pincode should not be empty")
+    .isLength({ min: 6, max: 6 })
+    .withMessage("Pincode should consist of 6 digits"),
+  body("age")
+    .not()
+    .isEmpty()
+    .withMessage("age should not be empty")
+    .isInt({ min: 1, max: 100 })
+    .withMessage("Age should between 1 to 100"),
+  body("gender").custom((value) => {
+    if (value == "Male" || value == "Female" || value == "Others") {
+      return true;  // return true is very important while writing custom validators
     }
     throw new Error("Please enter the valid gender");
+  }),
 
-}),
+  async (req, res) => {
+    try {
+      console.log(body("first_name")); //  esse you will get how many validators are there. Very Important
 
-
-
-async (req, res) => {
-    try{
-
-        console.log(body("first_name"))
-
-
-        const errors = validationResult(req);
+      const errors = validationResult(req);
       console.log({ errors });
       if (!errors.isEmpty()) {
         return res.status(400).send({ errors: errors.array() });
-
       }
 
+      const user = await User.create(req.body);
 
-const user= await User.create(req.body);
-
-return res.status(201).send({user: user}) 
+      return res.status(201).send({ user: user });
+    } catch (error) {
+      return res.status(500).send({ message: error });
     }
+  }
+);
 
-    catch(error){
-        return res.status(500).send({ message: error });
-    }
-})
-
-module.exports= router;
+module.exports = router;
